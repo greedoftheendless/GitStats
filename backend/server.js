770bb842cd138221620ca19caaf8bf155feed008
greedoftheendless <greedoftheendless@gmail.com>
2026-03-17 01:35:02 +0530

    change to port line

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ import express from 'express'
 import { Server } from 'socket.io'
 import cors from 'cors'
 
-const PORT = process.env.PORT || 3001
+const PORT = 3001
 
 const app = express()
 app.use(cors({ origin: '*' }))
